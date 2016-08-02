f20c3589e578d7d23590b3e2ae729fbb6e77cde1
njczy2010 <609738062@qq.com>
2016-08-02 11:16:21 +0800

    20160802

--- config.php
+++ config.php
@@ -2,7 +2,17 @@
 
 $G_ROOT = dirname(__FILE__);
 
-$G_CONFIG["host"] = array (
+##JSON format
+# {"remote_addr":"123.59.102.50","remote_user":"-","time_local":"2016-08-01T17:01:00+08:00","http_host":"e4fe210f8.bsclink.com","api_id":"193","api_path_id":"1153","caller_id":"0","method":"GET","request_uri":"/monitor?format=json&service=halfasync&nginx_ip=123.59.102.47","uri":"/monitor","request_time":"0.152","status":"200","upstream_addr":"124.239.189.15:8080","upstream_status":"200","upstream_response_time":"0.152","request_length":"120","body_bytes_sent":"38","http_referer":"-","http_user_agent":"-","http_x_forwarded_for":"-","upstream_cache_status":"MISS","hostname":"bgp-beijing-beijing-1-123-59-102-47"}
+
+# {"remote_addr":"$remote_addr","remote_user":"$remote_user","time_local":"$time_iso8601","http_host":"$http_host","api_id":"$api_id","api_path_id":"$api_path_id","caller_id":"$caller_id","method":"$request_method","request_uri":"$request_uri","uri":"$orignal_uri","request_time":"$request_time","status":"$status","upstream_addr":"$upstream_addr","upstream_status":"$upstream_status","upstream_response_time":"$upstream_response_time","request_length":"$request_length","body_bytes_sent":"$body_bytes_sent","http_referer":"$http_referer","http_user_agent":"$http_user_agent","http_x_forwarded_for":"$http_x_forwarded_for","upstream_cache_status":"$upstream_cache_status","hostname":"$hostname"}
+
+
+$G_CONFIG["remote_user"] = array (
+	"0" => "-",
+);
+
+$G_CONFIG["http_host"] = array (
 	"0" => "www.baidu.com",
 	"1" => "pubads.g.doubleclick.net",
 	"2" => "win.fcai.com",
@@ -15,7 +25,7 @@ $G_CONFIG["host"] = array (
 	"9" => "news.dayoo.com",
 );
 
-$G_CONFIG["method"] = array (
+$G_CONFIG["request_method"] = array (
 	"0" => "GET",
 	"1" => "POST",
 	"2" => "PUT",
@@ -26,11 +36,28 @@ $G_CONFIG["method"] = array (
 	"7" => "HEAD",
 );
 
-$G_CONFIG["ua"] = array (
-	"0" => "user agent",
+$G_CONFIG["request_uri"] = array (
+	"0" => "/monitor?format=json&service=halfasync&nginx_ip=123.59.102.47",
 );
 
-$G_CONFIG["refer"] = array (
-	"0" => "",
+$G_CONFIG["orignal_uri"] = array (
+	"0" => "/monitor",
 );
+
+$G_CONFIG["http_referer"] = array (
+	"0" => "-",
+);
+
+$G_CONFIG["http_user_agent"] = array (
+	"0" => "-",
+);
+
+$G_CONFIG["http_x_forwarded_for"] = array (
+	"0" => "-",
+);
+
+$G_CONFIG["upstream_cache_status"] = array (
+	"0" => "MISS",
+);
+
 ?>
--- produce.php
+++ produce.php
@@ -7,19 +7,54 @@ require_once("$root/config.php");
 $kafka = new Kafka("localhost:9092");
 
 function create_message() {	
-	$num = mt_rand(0,9);
-	$host = $GLOBALS["G_CONFIG"]["host"]["$num"];
+	$remote_addr = "" . mt_rand(0,255) . "." . mt_rand(0,255) . "." . mt_rand(0,255) . "." . mt_rand(0,255);
 
-	$client_ip = "" . mt_rand(0,255) . "." . mt_rand(0,255) . "." . mt_rand(0,255) . "." . mt_rand(0,255);
+	$remote_user = $GLOBALS["G_CONFIG"]["remote_user"]["0"];
+
+	$datetime = new DateTime('17 Oct 2008');
+	$time_iso8601 = $datetime->format('c');
+
+	$num = mt_rand(0,10);
+	$http_host = $GLOBALS["G_CONFIG"]["http_host"]["$num"];
+
+	$api_id = mt_rand(0,1000);
+
+	$api_path_id = mt_rand(0,100000);
+
+	$caller_id = mt_rand(0,1000);
 
 	$num2 = mt_rand(0,7);
-	$method = $GLOBALS["G_CONFIG"]["method"]["$num2"];
+	$request_method = $GLOBALS["G_CONFIG"]["request_method"]["$num2"];
 
-	$ua = $GLOBALS["G_CONFIG"]["ua"]["0"];
+	$num3 = mt_rand(0,0);
+	$request_uri = $GLOBALS["G_CONFIG"]["request_uri"]["$num3"];
 
-	$refer = $GLOBALS["G_CONFIG"]["refer"]["0"];
+	$num4 = mt_rand(0,0);
+	$orignal_uri = $GLOBALS["G_CONFIG"]["orignal_uri"]["$num4"];
 
-	$product = "/" . md5(mt_rand() );
+	$request_time = (double)mt_rand(0,1000) / 1000;
+
+	$status = 100 * mt_rand(2,5) + 10 + mt_rand(0,5);
+
+	$upstream_addr = "" . mt_rand(0,255) . "." . mt_rand(0,255) . "." . mt_rand(0,255) . "." . mt_rand(0,255) . ":8080";
+
+	$upstream_status = 100 * mt_rand(2,5) + 10 + mt_rand(0,5);
+
+	$upstream_response_time = (double)mt_rand(0,1000) / 1000;
+
+	$request_length = mt_rand(0,1000);
+
+	$body_bytes_sent = mt_rand(0,100);
+
+	$http_referer = $GLOBALS["G_CONFIG"]["http_referer"]["0"];
+
+	$http_user_agent = $GLOBALS["G_CONFIG"]["http_user_agent"]["0"];
+
+	$http_x_forwarded_for = $GLOBALS["G_CONFIG"]["http_x_forwarded_for"]["0"];
+
+	$upstream_cache_status = $GLOBALS["G_CONFIG"]["upstream_cache_status"]["0"];
+
+	$hostname = md5(mt_rand() );
 	$message = array (
 		"remote_addr" => $remote_addr,
 		"remote_user" => $remote_user,
